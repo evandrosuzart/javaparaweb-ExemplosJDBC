3a8f7d385590346dcb386043b7c7ec7e804d1a58
evandrosuzart <evandrossuzart@outlook.com>
2017-12-13 07:51:30 -0200

    criado classe para teste de conexao ao banco de dados

---
+++ src/main/java/br/com/javaparaweb/capitulo03/conexao/ConectaMySQL.java
@@ -0,0 +1,21 @@
+package br.com.javaparaweb.capitulo03.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectaMySQL {
+	public static void main(String[] args) {
+		Connection conexao = null;
+		try {
+			String url="jdbc:mysql://localhost/agenda";
+			String user = "newUser";
+			String pass = "newpass";
+			conexao=DriverManager.getConnection(url, user, pass);
+			System.out.println("Conectou");
+			conexao.close();
+		}catch(SQLException erro) {
+			System.out.println("Erro ao criar conexão, Erro: "+erro.getMessage());
+		}
+	}
+}
